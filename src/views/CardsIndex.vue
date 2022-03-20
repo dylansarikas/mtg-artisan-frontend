b589731b3d00905a09f0c9e4d3bf5313ab23b095
dylan <dylan.sarikas@gmail.com>
2022-03-20 00:09:12 -0500

    changes logic when searching for a card in card search and deck update

--- src/views/CardsIndex.vue
+++ src/views/CardsIndex.vue
@@ -7,6 +7,7 @@ export default {
       errors: [],
       cardSearch: "",
       cardImage: "",
+      newCardSearch: {},
     };
   },
   created: function () {
@@ -15,12 +16,23 @@ export default {
   methods: {
     indexCards: function () {
       axios
-        .get(`/cards?search=${this.cardSearch}`)
+        // .get(`/cards?search=${this.cardSearch}`)
+        // .get("/cards", this.newCardSearch)
+        .get("/cards", {
+          params: {
+            name: this.newCardSearch["cardName"],
+            color: this.newCardSearch["cardColor"],
+            text: this.newCardSearch["cardText"],
+            type: this.newCardSearch["cardType"],
+          },
+        })
         .then((response) => {
           console.log("cards index", response);
-          this.cards = response.data;
-          this.cardSearch = "";
-          this.cardImage = response.data["image_uris"]["small"];
+          // console.log(response.data.data[0]["image_uris"]["small"]);
+          this.cards = response.data.data;
+          console.log(this.cards[0]["name"]);
+          // this.cardSearch = "";
+          // this.cardImage = response.data["image_uris"]["small"];
         })
         .catch((error) => {
           console.log("card index error", error.response);
@@ -32,7 +44,51 @@ export default {
 </script>
 
 <template>
-  <div class="cards-index">
+  <div class="cards-search-index">
+    <h1>Search for a Card</h1>
+    <form v-on:submit.prevent="indexCards()">
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label for="cardName">Card Name:</label>
+        <input type="text" id="cardName" v-model="newCardSearch.cardName" />
+      </div>
+      <div>
+        <label for="cardColor">Card Color:</label>
+        <input type="radio" id="white" value="white" v-model="newCardSearch.cardColor" />
+        <label for="white">White</label>
+        <input type="radio" id="blue" value="blue" v-model="newCardSearch.cardColor" />
+        <label for="blue">Blue</label>
+        <input type="radio" id="black" value="black" v-model="newCardSearch.cardColor" />
+        <label for="black">Black</label>
+        <input type="radio" id="red" value="red" v-model="newCardSearch.cardColor" />
+        <label for="red">Red</label>
+        <input type="radio" id="green" value="green" v-model="newCardSearch.cardColor" />
+        <label for="green">Green</label>
+        <!-- <span>Picked: {{ picked }}</span> -->
+      </div>
+      <div>
+        <label for="cardText">Card Text:</label>
+        <input type="text" id="cardText" v-model="newCardSearch.cardText" />
+      </div>
+      <div>
+        <label for="cardType">Card Type:</label>
+        <input type="text" id="cardType" v-model="newCardSearch.cardType" />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+    <div v-if="!!cards[0]">
+      <div v-if="!!cards[0]['name']">
+        <div v-for="card in cards" v-bind:key="card.id">
+          <p>{{ card["name"] }}</p>
+          <img v-bind:src="`${card['image_uris']['small']}`" class="card-img-top" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="cards-index">
     <h1>Fuzzy Search for a Card</h1>
     <form v-on:submit.prevent="indexCards()">
       <ul>
@@ -48,21 +104,5 @@ export default {
       <img v-bind:src="cardImage" class="card-img-top" alt="" />
       <p>{{ cards }}</p>
     </div>
-  </div>
-  <!-- <div class="cards-index">
-    <h1>Card Search</h1>
-    <p>
-      Search:
-      <input type="text" id="search" v-model="cardSearch" />
-    </p> -->
-  <!-- <datalist id="cardNames">
-      <option v-for="card in cards" v-bind:key="card.id">
-        {{ card.name }}
-      </option>
-    </datalist> -->
-  <!-- <div>
-      <h2>{{ cards }}</h2>
-    </div> -->
-  <!-- <input type="submit" value="Submit" />
   </div> -->
 </template>
--- src/views/DecksShow.vue
+++ src/views/DecksShow.vue
@@ -5,10 +5,12 @@ export default {
     return {
       deck: {},
       errors: [],
-      newCardDeckParams: {},
       cards: [],
-      cardSearch: "",
-      cardImage: "",
+      newCardDeckParams: {},
+      deckCards: [],
+      // cardSearch: "",
+      // cardImage: "",
+      newCardParams: {},
     };
   },
   created: function () {
@@ -35,12 +37,21 @@ export default {
     },
     indexCards: function () {
       axios
-        .get(`/cards?search=${this.cardSearch}`)
+        .get("/cards", {
+          params: {
+            name: this.newCardParams["cardName"],
+            color: this.newCardParams["cardColor"],
+            text: this.newCardParams["cardText"],
+            type: this.newCardParams["cardType"],
+          },
+        })
         .then((response) => {
           console.log("cards index", response);
-          this.cards = response.data;
-          this.cardSearch = "";
-          this.cardImage = response.data["image_uris"]["small"];
+          // console.log(response.data.data[0]["image_uris"]["small"]);
+          this.cards = response.data.data;
+          // console.log(this.cards);
+          // this.cardSearch = "";
+          // this.cardImage = response.data["image_uris"]["small"];
         })
         .catch((error) => {
           console.log("card index error", error.response);
@@ -64,37 +75,47 @@ export default {
   </div>
   <div v-if="deck.owner" class="card-show">
     <p>Add Cards</p>
-    <form v-on:submit.prevent="createCardDeck()">
+    <form v-on:submit.prevent="indexCards()">
       <ul>
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
       </ul>
-      <div class="cards-index">
-        <h1>Fuzzy Search for a Card</h1>
-        <form v-on:submit.prevent="indexCards()">
-          <ul>
-            <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-          </ul>
-          Card Name:
-          <input type="text" v-model="cardSearch" />
-          <input type="submit" value="Search" />
-        </form>
-        <div v-if="!!cards['name']">
-          <!-- <p>{{ cards["name"] }}</p>
-          <p>{{ cards["image_uris"]["small"] }}</p> -->
-          <img v-bind:src="cardImage" class="card-img-top" alt="" />
-          <!-- <p>{{ cards }}</p> -->
+      <div>
+        <label for="cardName">Card Name:</label>
+        <input type="text" id="cardName" v-model="newCardParams.cardName" />
+      </div>
+      <div>
+        <label for="cardColor">Card Color:</label>
+        <input type="text" id="cardColor" v-model="newCardParams.cardColor" />
+      </div>
+      <div>
+        <label for="cardText">Card Text:</label>
+        <input type="text" id="cardText" v-model="newCardParams.cardText" />
+      </div>
+      <div>
+        <label for="cardType">Card Type:</label>
+        <input type="text" id="cardType" v-model="newCardParams.cardType" />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+    <div v-if="!!cards[0]">
+      <div v-if="!!cards[0]['name']">
+        <div v-for="card in cards" v-bind:key="card.id">
+          <p>{{ card["name"] }}</p>
+          <img v-bind:src="`${card['image_uris']['small']}`" class="card-img-top" alt="" />
         </div>
       </div>
-    </form>
+    </div>
   </div>
   <div class="cardDecks-new">
     <form v-on:submit.prevent="createCardDeck()">
       <ul>
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
       </ul>
-      <div v-if="!!cards['name']">
-        Click Here to Add Card
-        <input type="submit" value="Add" />
+      <div v-if="!!cards[0]">
+        <div v-if="!!cards[0]['name']">
+          Click Here to Add Card
+          <input type="submit" value="Add" />
+        </div>
       </div>
     </form>
   </div>
